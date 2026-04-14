2127ef47d6a53b020982c335658e12548500ae65
Shinu Cherian <shinucherian90@gmail.com>
2026-04-14 23:54:53 +0530

    Improved profile system with custom edit form and UI enhancements

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from django.urls import include
 from django.contrib.auth import views as auth_views
 from tracker import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('signup/', views.signup, name='signup'),
-]
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- tracker/forms.py
+++ tracker/forms.py
@@ -14,3 +14,9 @@ class ProfileForm(forms.ModelForm):
             'skills',
             'resume'
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['profile_pic'].required = False
+        self.fields['resume'].required = False
