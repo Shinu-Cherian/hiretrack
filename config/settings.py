00d2e9d6fd73e78671bf1f9d2bf371f10ae6e749
Shinu Cherian <shinucherian90@gmail.com>
2026-06-02 15:26:37 +0530

    fix: set DEFAULT_FROM_EMAIL to prevent Gmail SMTP from rejecting emails

--- config/settings.py
+++ config/settings.py
@@ -100,6 +100,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'support.hiretrack@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+SERVER_EMAIL = EMAIL_HOST_USER
 
 
 # Password validation
